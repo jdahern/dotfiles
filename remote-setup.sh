faa4998590510ae8102b2e34f4608e366e4a4cad
Jeff <jeffery.ahern@gmail.com>
2016-12-07 12:31:08 -0700

    Update remote-setup.sh

--- remote-setup.sh
+++ remote-setup.sh
@@ -8,6 +8,6 @@ if [ -z "$CMD" ]; then
 else
   echo "Installing dotfiles"
   mkdir -p "$HOME/dotfiles" && \
-  eval "$CMD https://github.com/nicksp/dotfiles/tarball/master | tar -xzv -C ~/.dotfiles --strip-components=1 --exclude='{.gitignore}'"
+  eval "$CMD https://github.com/jdahern/dotfiles/tarball/master | tar -xzv -C ~/.dotfiles --strip-components=1 --exclude='{.gitignore}'"
   . "$HOME/dotfiles/setup.sh"
 fi
